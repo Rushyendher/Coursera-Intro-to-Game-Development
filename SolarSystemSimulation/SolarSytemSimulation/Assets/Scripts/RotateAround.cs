d5eedccbe44963522b4b9ec591da38df09e48261
Rushyendher Reddy Sanku <reddyrushi20@gmail.com>
2016-12-17 18:03:00 +0530

    Add assignment-1 SolarSystem Simulation

---
+++ SolarSystemSimulation/SolarSytemSimulation/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	public Transform target;
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+		if (target == null) {
+			target = this.gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (target.transform.position, target.transform.up, speed * Time.deltaTime);
+	}
+}
